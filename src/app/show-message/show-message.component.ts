0eaf47a2925b01482bf82b5c379ceb9bf1ef65ba
Enda Lee <enda.lee@ittdublin.ie>
2016-11-07 13:52:06 +0000

    message box

---
+++ src/app/show-message/show-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-message',
+  templateUrl: './show-message.component.html',
+  styleUrls: ['./show-message.component.css']
+})
+export class ShowMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
